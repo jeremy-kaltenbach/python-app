6b862ba84d07a7b74e17a0a60e3758b85ddc3cfd
Jeremy Kaltenbach <jkaltenbach@gmail.com>
2026-09-09 15:53:08 -0400

    Initial commit

---
+++ src/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+from datetime import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/details')
+def details():
+    return jsonify({
+        'time': datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
+        'hostname': socket.gethostname()
+    })
+
+@app.route('/api/v1/healthz')
+def health():
+    return jsonify({
+        'status': 'Up'
+    }), 200
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
