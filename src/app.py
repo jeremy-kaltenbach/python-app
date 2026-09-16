9b9a79df7153042c6f6a96524e4deb9ab39226f0
jeremy-kaltenbach <jkaltenbach@gmail.com>
2026-09-16 17:00:51 -0400

    Update login command again

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ def details():
     return jsonify({
         'time': datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
         'hostname': socket.gethostname(),
-        'message': 'My spoon is too big!'
+        'message': 'My spoon is too big!!'
     })
 
 @app.route('/api/v1/healthz')
